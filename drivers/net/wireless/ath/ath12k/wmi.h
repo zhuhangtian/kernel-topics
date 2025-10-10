7d0484de50d924342b1b6ee5e328a46e2b5d8a09
Jeff Johnson <jeff.johnson@oss.qualcomm.com>
2025-10-10 09:47:58 -0700

    wifi: ath12k: Remove struct wmi_bcn_send_from_host_cmd
    struct wmi_bcn_send_from_host_cmd is unused, so remove it.
    
    Compile tested only.
    
    Link: https://patch.msgid.link/20251010-ath12k-nuke-wmi_bcn_send_from_host_cmd-v1-1-6f1172b77848@oss.qualcomm.com
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath12k/wmi.h
+++ drivers/net/wireless/ath/ath12k/wmi.h
@@ -3612,20 +3612,6 @@ struct ath12k_wmi_scan_cancel_arg {
 	u32 pdev_id;
 };
 
-struct wmi_bcn_send_from_host_cmd {
-	__le32 tlv_header;
-	__le32 vdev_id;
-	__le32 data_len;
-	union {
-		__le32 frag_ptr;
-		__le32 frag_ptr_lo;
-	};
-	__le32 frame_ctrl;
-	__le32 dtim_flag;
-	__le32 bcn_antenna;
-	__le32 frag_ptr_hi;
-};
-
 #define WMI_CHAN_INFO_MODE		GENMASK(5, 0)
 #define WMI_CHAN_INFO_HT40_PLUS		BIT(6)
 #define WMI_CHAN_INFO_PASSIVE		BIT(7)
